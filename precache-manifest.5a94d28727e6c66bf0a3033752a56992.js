a02e56ac0eab37c552072f04caf27b45e41fb310
Amit Maurya <amit.maurya@ltts.com>
2020-08-03 03:02:39 +0530

    Updates

---
+++ precache-manifest.5a94d28727e6c66bf0a3033752a56992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22f94781d90884a2579e3decf525fdc",
+    "url": "/amitmaury/upstox-test/index.html"
+  },
+  {
+    "revision": "f2123f75075627c24670",
+    "url": "/amitmaury/upstox-test/static/css/main.b096d558.chunk.css"
+  },
+  {
+    "revision": "6e706f76f1e330844e91",
+    "url": "/amitmaury/upstox-test/static/js/2.d0f1f50e.chunk.js"
+  },
+  {
+    "revision": "a6c07ce92e9d8d498b5ade08b1bd38bf",
+    "url": "/amitmaury/upstox-test/static/js/2.d0f1f50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2123f75075627c24670",
+    "url": "/amitmaury/upstox-test/static/js/main.79ec359d.chunk.js"
+  },
+  {
+    "revision": "0973b52d88e9444b13f5",
+    "url": "/amitmaury/upstox-test/static/js/runtime-main.5092b9e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/upstox-test/precache-manifest.4f81609d5e875283b8ba8fb6d673d65c.js"
+  "/amitmaury/upstox-test/precache-manifest.5a94d28727e6c66bf0a3033752a56992.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/upstox-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/amitmaury/upstox-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
