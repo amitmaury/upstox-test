f2318c5fc0bf781a34efa4d4516928486f19056b
Amit Maurya <amit.maurya@ltts.com>
2020-08-03 02:36:01 +0530

    first commit

--- src/services/historical.service.js
+++ src/services/historical.service.js
@@ -1,25 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 const WEB_SOCKET_URL = "http://kaboom.rksv.net/watch";
 
 export const getAllHistoricalData = () => {
-
-    return axios(`${process.env.REACT_APP_TEST_VAR}historical`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-            'Accept-Encoding': 'gzip, deflate'
-        
-        }
-    })
-        .then(response => response.data)
-        .catch(error => {
-            throw error;
-        });
+  return axios(`${process.env.REACT_APP_TEST_VAR}historical`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      "Accept-Encoding": "gzip, deflate",
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 export const getLiveChartData = () => {
-
-    return {
-        url:WEB_SOCKET_URL
-    };
-    
+  return {
+    url: WEB_SOCKET_URL,
+  };
 };
